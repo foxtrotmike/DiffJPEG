3890de47e3049e727d93f92c44b46c1b5e17294d
Fayyaz ul Amir Afsar Minhas <foxtrotmike@users.noreply.github.com>
2021-09-30 19:00:34 +0100

    Added by Fayyaz

---
+++ diffjpeg_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 17:15:12 2021
+
+@author: fayya
+"""
+
+
+import torch
+from DiffJPEG import DiffJPEG
+import numpy as np
+from skimage import data
+original = data.astronaut()/255.0
+jpeg = DiffJPEG(height=512, width=512, differentiable=True, quality=5)
+x0 = torch.from_numpy(original).float()
+x0 = torch.unsqueeze(x0,dim=0)
+x = x0.permute((0,3,1,2))
+xp = jpeg(x)
+xp0 = xp.permute((0,2,3,1))
+xp0n = xp0.detach().numpy()[0]
+x0n = x0.detach().numpy()[0]
+print(np.linalg.norm(x0n-xp0n))
